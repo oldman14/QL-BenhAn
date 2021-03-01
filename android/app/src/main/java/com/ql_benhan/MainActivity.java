392fe57ae0954441d822424ed6912fded54075bf
ngo hoang thai <thaibuaplol@gmail.com>
2021-03-01 22:28:12 +0700

    first commit

---
+++ android/app/src/main/java/com/ql_benhan/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ql_benhan;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "QL_BenhAn";
+  }
+}
